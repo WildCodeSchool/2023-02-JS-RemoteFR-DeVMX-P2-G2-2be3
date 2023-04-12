95f0e46ce704e0a9132d115c6fbe2e0408e16a63
AnthonyLASTERNAS <lespike24@hotmail.fr>
2023-04-12 18:12:58 +0200

    searchbar added plus basket and shop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/Home";
-
+import Shop from "./pages/Shop";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Shop />
       <p>coucou</p>
     </div>
   );
