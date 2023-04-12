7ba0a48d66c70c0ce8a1c29c38321c193270eddf
Frederico Cassola <cfcassola@gmail.com>
2023-04-12 23:23:09 +0200

    Add type to button

--- frontend/src/components/itemCard/ItemCardQuantityButton.jsx
+++ frontend/src/components/itemCard/ItemCardQuantityButton.jsx
@@ -1,5 +1,10 @@
-function ItemCardQuantityButton({ icon }) {
-  return <button className="material-symbols-outlined">{icon}</button>;
+/* eslint-disable react/button-has-type */
+function ItemCardQuantityButton(icon) {
+  return (
+    <button type="subit" className="material-symbols-outlined">
+      {icon}
+    </button>
+  );
 }
 
 export default ItemCardQuantityButton;
