d1910df37f527c49194a5c195203bebda06ba08f
Anne LOUIS <anne.louis51100@gmail.com>
2023-04-14 12:00:23 +0200

    BasketReceipt.css added and changes in BasketReceipt.jsx

---
+++ frontend/src/components/basket/BasketReceipt.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line import/no-unresolved
+import logoNutridriveNB from "@assets/logoNutridriveNB.png";
+import qrcode from "@assets/qrcode.png";
+
+function Receipt() {
+  return (
+    <div className="receiptContainer">
+      <img
+        className="img-nutridrive_logo"
+        src={logoNutridriveNB}
+        alt="Logo NutriDrive noir et blanc"
+      />
+      <h2>Vous avez été reçu par Micheline</h2>
+      <div className="receipt-details">
+        <p>Pizza, tomate, mozza............x1</p>
+        <p>Céréales Bjorg, flocon..........x2</p>
+        <p>Pizza, tomate, mozza............x1</p>
+        <p>Céréales Bjorg, flocon..........x2</p>
+        <p>Pizza, tomate, mozza............x1</p>
+        <p>Céréales Bjorg, flocon..........x2</p>
+        <p>Pizza, tomate, mozza............x1</p>
+        <p>Céréales Bjorg, flocon..........x2</p>
+        <p>Pizza, tomate, mozza............x1</p>
+        <p>Céréales Bjorg, flocon..........x2</p>
+      </div>
+      <img className="img-qrcode" src={qrcode} alt="QR Code" />
+      <h1>MERCI DE VOTRE VISITE</h1>
+      <h1>A BIENTÔT</h1>
+    </div>
+  );
+}
+
+export default Receipt;
--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable import/no-unresolved */
 import ButtonBasket from "@components/ButtonBasket";
 import ButtonReturn from "@components/basket/ButtonReturn";
-import Receipt from "@components/basket/Receipt";
+import Receipt from "@components/basket/BasketReceipt";
 import ButtonValide from "@components/basket/ButtonValide";
+import "../style/BasketReceipt.css";
 
 function Basket() {
   return (
