45801c6310e54271b6e9e42d68b09c65ad84f92d
Anne LOUIS <anne.louis51100@gmail.com>
2023-04-14 08:39:00 +0200

    Receipt and logos NutridriveNB qrcode added

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-unresolved */
 import ButtonBasket from "@components/ButtonBasket";
+import Receipt from "@components/basket/Receipt";
 
 function Basket() {
   return (
     <header>
       <ButtonBasket />
+      <Receipt />
     </header>
   );
 }
