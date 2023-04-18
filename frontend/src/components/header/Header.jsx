5f31b0482b464ab2597efb68341a880ac0096c70
Anne LOUIS <anne.louis51100@gmail.com>
2023-04-18 07:29:31 +0200

    Add CSS header and background image in shop and basket

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,17 +1,15 @@
 /* eslint-disable import/no-unresolved */
-// eslint-disable-next-line prettier/prettier
-import React from "react";
-// eslint-disable-next-line prettier/prettier
-import ButtonBasket from "./ButtonBasket";
-// eslint-disable-next-line import/order
 import ImageLogo from "@assets/NutriDrive_02_02.jpeg";
+import ButtonBasket from "./ButtonBasket";
 
 function Header() {
   return (
     <header>
-      <img src={ImageLogo} alt="" />
-      <h3>Bonjour Toto</h3>
-      <ButtonBasket />
+      <img className="imgLogoHeader" src={ImageLogo} alt="Logo NutriDrive" />
+      <div className="idBasket">
+        <h3>Bonjour Toto</h3>
+        <ButtonBasket />
+      </div>
     </header>
   );
 }
--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -4,11 +4,14 @@ import NetworkFooter from "@components/NetworkFooter";
 import ButtonReturn from "@components/basket/ButtonReturn";
 import Receipt from "@components/basket/BasketReceipt";
 import ButtonValide from "@components/basket/ButtonValide";
-import "../style/BasketReceipt.css";
+
+import "../style/Basket/Basket.css";
+import "../style/Basket/BasketReceipt.css";
+import "../style/Header.css";
 
 function Basket() {
   return (
-    <div>
+    <div className="basket">
       <Header />
       <ButtonReturn />
       <Receipt />
