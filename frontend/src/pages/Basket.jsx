c0ea86dbe6df076e006816a9f18a852176e27c9d
Frederico Cassola <cfcassola@gmail.com>
2023-04-20 16:46:35 +0200

    Remove modal bugs and eslint path rules

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-unresolved */
-import Header from "@components/header/Header";
-import NetworkFooter from "@components/NetworkFooter";
-import ButtonReturn from "@components/basket/ButtonReturn";
-import Receipt from "@components/basket/BasketReceipt";
-import ButtonValide from "@components/basket/ButtonValide";
-import ModalMessage from "@components/ModalMessage";
+import Header from "../components/header/Header";
+import NetworkFooter from "../components/NetworkFooter";
+import ButtonReturn from "../components/basket/ButtonReturn";
+import Receipt from "../components/basket/BasketReceipt";
+import ButtonValide from "../components/basket/ButtonValide";
+import ModalMessage from "../components/basket/ModalMessage";
 
 import "../style/Basket/Basket.css";
 import "../style/Basket/BasketReceipt.css";
