2b2a4b80c9d03f77fc962018d6d717c6cd7b64b7
MarineDeveza <marine.deveza@gmail.com>
2023-04-20 18:28:25 +0200

    marine_u.3.2.2 buttonValide-with-modal

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -3,7 +3,6 @@ import NetworkFooter from "../components/NetworkFooter";
 import ButtonReturn from "../components/basket/ButtonReturn";
 import Receipt from "../components/basket/BasketReceipt";
 import ButtonValide from "../components/basket/ButtonValide";
-import ModalMessage from "../components/basket/ModalMessage";
 
 import "../style/Basket/Basket.css";
 import "../style/Basket/BasketReceipt.css";
@@ -16,7 +15,6 @@ function Basket() {
       <ButtonReturn />
       <Receipt />
       <ButtonValide />
-      <ModalMessage />
       <NetworkFooter />
     </div>
   );
