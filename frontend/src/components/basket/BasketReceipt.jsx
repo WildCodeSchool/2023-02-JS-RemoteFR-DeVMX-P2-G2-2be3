53c04c09a051f25db23f52b14acb80051d7e8527
Anne LOUIS <anne.louis51100@gmail.com>
2023-05-03 11:55:26 +0200

    Change layout on basket receipt + add margi bottom qr code

--- frontend/src/components/basket/BasketReceipt.jsx
+++ frontend/src/components/basket/BasketReceipt.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { PropTypes } from "prop-types";
 import QRcode from "qrcode";
 import logoNutridriveNB from "../../assets/logoNutridriveNB.png";
-import BasketReceiptQuantityButtons from "./BasketReceiptQuantityButtons";
+import ItemCardQuantityButton from "../shop/itemCard/ItemCardQuantityButton";
 
 function Receipt({ cartItems, handleRemoveItem, handleAddItem }) {
   const [qrcode, setQrcode] = useState("");
@@ -42,19 +42,29 @@ function Receipt({ cartItems, handleRemoveItem, handleAddItem }) {
       <div className="receipt-details">
         {cartItems &&
           cartItems.map((item) => (
-            <div className="item-receipt" key={item.id}>
-              <span className="item-receipt-title">{item.product_name_fr}</span>
-              <span className="item-receipt-space">........</span>
-              <span className="item-receipt-quantity">{item.quantity}</span>
-              <BasketReceiptQuantityButtons
-                handleRemoveItem={handleRemoveItem}
-                handleAddItem={handleAddItem}
-                cartItems={cartItems}
-                itemInReceipt={item}
-              />
+            <div key={item.id}>
+              <div className="description-quantity-items">
+                <span>{item.product_name_fr.substr(0, 30)}</span>
+                <span>
+                  {".".repeat(
+                    35 -
+                      item.product_name_fr.substr(0, 30).length -
+                      item.quantity.toString().length
+                  )}
+                </span>
+                <span>{item.quantity}</span>
+              </div>
+              <div className="button-change-quantity">
+                <ItemCardQuantityButton
+                  product={item}
+                  handleRemoveItem={handleRemoveItem}
+                  handleAddItem={handleAddItem}
+                />
+              </div>
             </div>
           ))}
       </div>
+
       {totalItemsQuantity !== 0 && (
         <span className="total-quantity-receipt">
           Quantité totale: {totalItemsQuantity}
